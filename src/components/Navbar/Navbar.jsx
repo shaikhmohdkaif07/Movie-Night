2ea5805ed4f8dfa99b690e86ed0ea78d8b3d1fdd
shaikhmohdkaif07 <shaikhmohdkaif1977@gmail.com>
2025-09-26 20:20:40 +0530

    Update security details and README with project details

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,7 +9,7 @@ import Party from "../../assets/partying-face.png";
 const Navbar = () => {
     return (
         <nav className='navbar'>
-            <h1>MovieManiac</h1>
+            <h1>Movie Night</h1>
 
             <div className='navbar_links'>
                 <DarkMode />
